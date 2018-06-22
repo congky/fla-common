1ada98e500e329da2a3ff2c959222f033f0ee0e9
Congky <cokycongky@gmail.com>
2018-06-22 14:03:40 +0700

    enhance common service provider

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -2,6 +2,7 @@
 namespace FLA\Common;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class CommonServiceProvider extends ServiceProvider
 {
@@ -12,5 +13,9 @@ class CommonServiceProvider extends ServiceProvider
     public function boot()
     {
         require __DIR__ . '/Routes/api.php';
+
+        // map api route
+        Route::prefix('api')
+            ->group(__DIR__ . '/Routes/api.php');
     }
 }
